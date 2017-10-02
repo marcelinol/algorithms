3f1794be106d75be2dc2555aacf3637a774b37ab
marcelinol <marcelinolucianom@gmail.com>
2017-10-01 21:46:52 -0300

    Handle collisions by adding value to the next
    position in the array.
    Here are some known issues:
    - I can't search for data precisely if I have had a collision before
    - I would prefer to use a linked list to handle the collisions

--- hash/hash.go
+++ hash/hash.go
@@ -8,17 +8,27 @@ import (
 const hashSize int = 30
 
 type HashTable struct {
-	// hashFunction func(string) int
 	length  int
 	storage []string
 }
 
 func NewHashTable() *HashTable {
+	// Why can't I create the strings array like this?
+	// [hashSize]string{}
 	return &HashTable{0, make([]string, hashSize)}
 }
 
 func (h *HashTable) Add(key string, value string) error {
 	index := hashFunction(key)
+
+	for h.storage[index] != "" {
+		fmt.Printf("Collision on index ")
+		fmt.Println(index)
+		index++
+	}
+
+	fmt.Printf("Adding value " + value + " to index ")
+	fmt.Println(index)
 	h.storage[index] = value
 	h.length++
 	return nil
@@ -26,20 +36,25 @@ func (h *HashTable) Add(key string, value string) error {
 
 func hashFunction(key string) int {
 	value, _ := strconv.Atoi(key)
-	return value
+	return value % 31
 }
 
 func main() {
 	h := NewHashTable()
 	fmt.Println(h)
 
-	fmt.Printf("Adding value 1 to hash\n")
 	h.Add("1", "first value")
 	fmt.Println(h)
 
-	fmt.Printf("Adding value 23 to hash\n")
-	h.Add("23", "xunda")
+	h.Add("20", "xunda")
 	fmt.Println(h)
+
+	h.Add("50", "xunda2") // It should cause a collision with the "xunda" item
+	fmt.Println(h)
+
+	h.Add("20", "this should overwrite the item xunda")
+	fmt.Println(h)
+
 }
 
 // Questions
