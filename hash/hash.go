060d3928934699b95651c09295f17e9b22a7700a
marcelinol <marcelinolucianom@gmail.com>
2017-10-04 22:28:22 -0300

    (wip) Use my go linked list
    Not working yet

--- hash/hash.go
+++ hash/hash.go
@@ -7,172 +7,85 @@ import (
 
 // Linked List
 
-type node struct {
-	key   string
-	value string
-	next  *node
+type Node struct {
+	item     interface{}
+	nextNode *Node
 }
 
-type linkedList struct {
-	first node
-	len   int
+type LinkedList struct {
+	head   *Node
+	length int
 }
 
-func (n node) getKey() string {
-	return n.key
+func NewLinkedList() *LinkedList {
+	node := Node{nil, nil}
+
+	return &LinkedList{&node, 0}
 }
 
-func (n node) getValue() string {
-	return n.value
+func (l *LinkedList) AddFirst(item interface{}) {
+	newNode := Node{item, l.head}
+	l.head = &newNode
+	l.length++
 }
 
-func (n *node) setKey(key string) {
-	n.key = key
-}
-
-func (n *node) setValue(value string) {
-	n.value = value
-}
-
-func (n node) getNext() node {
-	if n.next == nil {
-		return node{}
-	} else {
-		return *n.next
-	}
-}
-
-func (n *node) setNext(value node) {
-	n.next = &value
-}
-
-func (list *linkedList) add(key string, value string, index int) {
-
-	if index >= 0 {
-		list_node := node{key, value, nil}
-
-		if list.empty() {
-			list.first = list_node
-		} else {
-			if index == 0 {
-				// insert in first
-				list_node.setNext(list.first)
-				list.first = list_node
-			} else {
-				prev_node := list.first
-				curr_node := list.first.getNext()
-				curr_index := 1
-
-				for curr_node.next != nil {
-
-					if curr_index == index {
-						list_node.setNext(curr_node)
-						prev_node.setNext(list_node)
-						break
-					}
-
-					prev_node = curr_node
-					curr_node = curr_node.getNext()
-					curr_index++
-				}
-			}
+func (l *LinkedList) include(item interface{}) bool {
+	currentNode := l.head
+	for currentNode.nextNode != nil {
+		if currentNode.item == item {
+			return true
 		}
-		list.len++
+		currentNode = currentNode.nextNode
 	}
-}
 
-func (list linkedList) remove(index int) {
-
-	if !list.empty() && index >= 0 && index < list.len {
-		flag_remove := false
-
-		if list.first.next == nil {
-			list.first = node{}
-			flag_remove = true
-		} else if index == 0 {
-			list.first = list.first.getNext()
-			flag_remove = true
-		} else {
-			prev_node := list.first
-			curr_node := list.first.getNext()
-			curr_index := 1
-
-			for curr_node.next != nil {
-				if index == curr_index {
-					next_node := curr_node.getNext()
-					prev_node.setNext(next_node)
-					curr_node.setNext(node{})
-					flag_remove = true
-					break
-				}
-
-				prev_node = curr_node
-				curr_node = curr_node.getNext()
-				curr_index++
-			}
-		}
-
-		if flag_remove == true {
-			list.len--
-		}
-	}
-}
-
-func (list linkedList) empty() bool {
-	if list.len == 0 {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (list linkedList) show() {
-	curr_node := list.first
-
-	for curr_node.next != nil {
-		fmt.Println(curr_node.getKey())
-		fmt.Println(curr_node.getValue())
-		curr_node = curr_node.getNext()
-		fmt.Println("Final:", curr_node)
-	}
+	return false
 }
 
 // HashTable
 
 const hashSize int = 30
 
+type HashItem struct {
+	key   string
+	value string
+}
+
 type HashTable struct {
 	length  int
-	storage [hashSize]linkedList
+	storage [hashSize]LinkedList
 }
 
 func NewHashTable() *HashTable {
-	// Why can't I create the strings array like this?
-	// [hashSize]string{}
-	return &HashTable{0, [hashSize]linkedList{}}
+	return &HashTable{0, [hashSize]LinkedList{}}
 }
 
-func (h *HashTable) Add(key string, value string) error {
+func (h *HashTable) Add(key string, value string) {
 	index := hashFunction(key)
 
 	fmt.Printf("Adding value " + value + " to index ")
 	fmt.Println(index)
 	fmt.Println(h.storage[index])
-	h.storage[index].add(key, value, 0)
+
+	newItem := HashItem{key, value}
+
+	h.storage[index].AddFirst(newItem)
 	h.length++
-	return nil
 }
 
 func (h *HashTable) Search(key string) string {
 	index := hashFunction(key)
 
-	currNode := h.storage[index].first
+	currentNode := h.storage[index].head
 
-	for currNode.getKey() != key {
-		currNode = currNode.getNext()
+	for currentNode.nextNode != nil {
+		if currentNode.item.key == key {
+			return currentNode.item.value
+		}
+		currentNode = currentNode.nextNode
 	}
 
-	return currNode.value
+	item := currentNode.item
+	return item.value
 }
 
 func hashFunction(key string) int {
